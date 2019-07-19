95d89fc36f116abec3923ee0bf15deb490476cbd
imperative-cron-3751 <mikelim724@gmail.com>
2019-07-19 23:30:40 +0000

    Done.

--- hoisting.js
+++ hoisting.js
@@ -3,28 +3,30 @@ function callMe() {
   console.log("and this is crazy..");
   console.log("but here's my number..");
   console.log("so call me");
+  var lyric = "maybe"; //place before return statement
   return lyric;
-  var lyric = "maybe";
+
 }
 
 
 function crazy() {
   // fix the code in here:
-  thisIsCrazy();
 
   var thisIsCrazy = function (){
     console.log("hey!!!")
   }
+
+  thisIsCrazy(); // call function after because before it was declare thisiscrazy, then called nothing, then assigned it a function
 }
 
 function sayMyName() {
   // fix the code in here:
   var name = "Cricky";
-
+  function sayMy() {
+    var name = "Kristin"; //put this before console log so it prints the local variable of the name rather than global
+    console.log(name);
+  }
   sayMy();
 
-  function sayMy() {
-    console.log(name);
-    var name = "Kristin";
-  }
+
 }
