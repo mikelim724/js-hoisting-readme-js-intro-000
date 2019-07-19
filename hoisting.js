a8386246dfb3302a15ed95363a159dfa19bb9781
imperative-cron-3751 <mikelim724@gmail.com>
2019-07-19 23:20:12 +0000

    Automatically backed up by Learn

--- hoisting.js
+++ hoisting.js
@@ -3,28 +3,30 @@ function callMe() {
   console.log("and this is crazy..");
   console.log("but here's my number..");
   console.log("so call me");
+  var lyric = "maybe"; //place before return statement
   return lyric;
-  var lyric = "maybe";
+
 }
 
 
 function crazy() {
   // fix the code in here:
-  thisIsCrazy();
 
   var thisIsCrazy = function (){
     console.log("hey!!!")
   }
+
+  thisIsCrazy(); // call function after because before it was declare thisiscrazy, then called nothing, then assigned it a function
 }
 
 function sayMyName() {
   // fix the code in here:
   var name = "Cricky";
-
-  sayMy();
-
   function sayMy() {
     console.log(name);
     var name = "Kristin";
   }
+  sayMy();
+
+
 }
